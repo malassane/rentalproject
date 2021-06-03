7a740f885d5507e4488abd3e8aca7389c7365f49
hado anselme <anselmehado85@yahoo.com>
2021-06-03 16:25:04 +0100

    contoller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,19 +1,67 @@
 class PropertiesController < ApplicationController
+
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
+
   def index
-  end
-
-  def show
-  end
-
-  def update
-  end
-
-  def destroy
+    @properties = Property.all
   end
 
   def new
+    @property = Property.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+      else
+        if @post.save
+          redirect_to posts_path, notice: "ブログを作成しました！"
+        else
+          render :new
+        end
+      end
+    end
+
+  def show
+
   end
 
   def edit
+
+  end
+
+  def destroy
+    @property = Property.find(params[:id])
+    if @property.delete
+      flash[:notice] = 'station deleted!'
+      redirect_to root_path
+    else
+      flash[:error] = 'Failed to delete this property!'
+      render :destroy
+  end
+
+  def update
+    #@property = Property.find(params[:id])   # Overlap!
+    if @property.update(property_params)
+      redirect_to properties_path, notice: "I edited the property!"
+    else
+      render :edit
+    end
+  end
+
+
+  def set_post
+        @property = Property.find(params[:id])
+  end
+
+  def destroy
+    @property.destroy
+    redirect_to properties_path, notice:"I deleted the property!"
+  end
+
+  private
+  def property_params
+    params.require(:property).permit(:propertyName, :rent, :railName, :buildAge, :address, :note)
   end
 end
--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,19 +1,70 @@
 class StationsController < ApplicationController
+
+  before_action :set_station, only: [:show, :edit, :update, :destroy]
+
   def index
-  end
-
-  def show
-  end
-
-  def update
-  end
-
-  def destroy
+    @stations = Station.all
   end
 
   def new
+    @station = Station.new
+  end
+
+
+  def create
+  @station = Station.new(station_params)
+  if params[:back]
+    render :new
+  else
+  if @station.save
+    redirect_to stations_path, notice: "I have created a station!"
+  else
+    render :new
+  end
+  end
+
+  def show
+
   end
 
   def edit
+
   end
+
+
+
+  def update
+  @station = Station.find(params[:id])   # Overlap!
+  if @station.update(station_params)
+    redirect_to stations_path, notice: "I edited the station!"
+  else
+    render :edit
+  end
+  end
+
+  def destroy
+    @station = Station.find(params[:id])
+    if @station.delete
+      flash[:notice] = 'station deleted!'
+      redirect_to root_path
+    else
+      flash[:error] = 'Failed to delete this station!'
+      render :destroy
+    end
+
+    def set_post
+          @station = Station.find(params[:id])
+    end
+
+    def destroy
+      @station.destroy
+      redirect_to stations_path, notice:"I deleted the property!"
+    end
+
+    private
+    def station_params
+      params.require(:station).permit(:stationName, :timeWalk, :railName, :railName)
+    end
+
+
 end
